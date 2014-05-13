b18364d52104c06bf9446419e6137fbb459e40a3
Jonathan Linowes <linojon@gmail.com>
2014-05-12 21:08:56 -0400

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 BlocBlocit::Application.routes.draw do
+  get "posts/index"
   devise_for :users
-  resources :users, only: [:show, :update]
+  resources :users, only: [:index, :show, :update]
 
+  resources :posts, only: [:index]
+  
   resources  :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
